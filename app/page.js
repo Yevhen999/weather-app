d4f842e28ddd3f2667e1635833e59c76f479c51e
unknown <leshchenkoyevhen@gmail.com>
2023-07-27 08:53:00 +0800

    add text

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>Weather</p>
+        <p>Weather app</p>
       </div>
     </main>
   );
