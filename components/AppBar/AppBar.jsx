1d28eae34817383e8b83df29cd72603f9e7dd043
unknown <leshchenkoyevhen@gmail.com>
2023-07-31 22:29:06 -0400

    add container as a component

--- components/AppBar/AppBar.jsx
+++ components/AppBar/AppBar.jsx
@@ -1,21 +1,24 @@
 import { BiMenu, BiDotsVerticalRounded } from "react-icons/bi";
+import { Container } from "../Container/Container";
 
 export const AppBar = () => {
   return (
-    <div>
+    <Container>
       <div>
-        <BiMenu />
-        <h1>Toronto, Canada</h1>
-        <BiDotsVerticalRounded />
+        <div>
+          <BiMenu />
+          <h1>Toronto, Canada</h1>
+          <BiDotsVerticalRounded />
+        </div>
+        <nav>
+          <ul>
+            <li>Today</li>
+            <li>Forecast</li>
+            <li>Precipitation</li>
+            <li>Raduis</li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li>Today</li>
-          <li>Forecast</li>
-          <li>Precipitation</li>
-          <li>Raduis</li>
-        </ul>
-      </nav>
-    </div>
+    </Container>
   );
 };
