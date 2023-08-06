9dc432bbd63fd5d24543a2852ad059d5e8c63976
unknown <leshchenkoyevhen@gmail.com>
2023-08-06 18:28:32 -0400

    add styles for active link

--- components/AppBar/AppBar.jsx
+++ components/AppBar/AppBar.jsx
@@ -1,10 +1,16 @@
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import { Header } from "../Header/Header";
+import styles from "./AppBar.module.css";
 
 export const AppBar = () => {
   const [weather, setWeather] = useState({});
+
+  const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     const currentWeather = async () => {
@@ -28,15 +34,49 @@ export const AppBar = () => {
     <header>
       <Header weather={weather} />
       <nav>
-        <ul>
-          <li>
-            <Link href="/">Today</Link>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link
+              href="/"
+              className={
+                pathname == "/" ? styles.activeNavLink : styles.navLink
+              }
+            >
+              Today
+            </Link>
           </li>
-          <li>
-            <Link href="/forecast">Forecast</Link>
+          <li className={styles.navItem}>
+            <Link
+              href="/forecast"
+              className={
+                pathname == "/forecast" ? styles.activeNavLink : styles.navLink
+              }
+            >
+              Forecast
+            </Link>
           </li>
-          <li>Precipitation</li>
-          <li>Radar Sun & Moon</li>
+          <li className={styles.navItem}>
+            <Link
+              href="/precipitation"
+              className={
+                pathname == "/precipitation"
+                  ? styles.activeNavLink
+                  : styles.navLink
+              }
+            >
+              Precipitation
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link
+              href="/radar"
+              className={
+                pathname == "/radar" ? styles.activeNavLink : styles.navLink
+              }
+            >
+              Radar Sun & Moon
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
