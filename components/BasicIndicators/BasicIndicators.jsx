584a1f773e1cf6756dce8e50e0dc4f1626f36105
unknown <leshchenkoyevhen@gmail.com>
2023-08-12 16:24:39 -0400

    add content to Basic Indicators

--- components/BasicIndicators/BasicIndicators.jsx
+++ components/BasicIndicators/BasicIndicators.jsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import sunny from "../../public/sunny.svg";
+
 export const BasicIndicators = ({ current }) => {
-  return <div>{current.temp_c}</div>;
+  console.log(current);
+  return (
+    <div>
+      <div>
+        <Image src={sunny} alt="sunny" />
+        <p>{current.temp_c}&#176; | Feels like ...</p>
+      </div>
+      <div>
+        <span>{current.temp_c}&#176;</span>
+        <p>Partly cloudy</p>
+        <p>Wind</p>
+      </div>
+    </div>
+  );
 };
--- components/WeatherDate/WeatherDate.jsx
+++ components/WeatherDate/WeatherDate.jsx
@@ -4,7 +4,9 @@ export const WeatherDate = ({ weather }) => {
   return (
     <div className={styles.generalWrapper}>
       <div className={styles.dateWrapper}>
-        <h2 className={styles.title}>{weather.localtime}</h2>
+        <h2 className={styles.title}>
+          <time dateTime={weather.localtime}>{weather.localtime}</time>
+        </h2>
       </div>
     </div>
   );
