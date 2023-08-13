fd0a1747ffd774986cac3d2e2c6fa2969e8f9c07
unknown <leshchenkoyevhen@gmail.com>
2023-08-12 21:47:32 -0400

    add styles to basicIndicators

--- components/BasicIndicators/BasicIndicators.jsx
+++ components/BasicIndicators/BasicIndicators.jsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import sunny from "../../public/sunny.svg";
+import styles from "./BasicIndicators.module.css";
 
 export const BasicIndicators = ({ current }) => {
   console.log(current);
   return (
-    <div>
-      <div>
+    <div className={styles.wrapper}>
+      <div className={styles.contentWrapper}>
         <Image src={sunny} alt="sunny" />
-        <p>{current.temp_c}&#176; | Feels like ...</p>
+        <p className={styles.leftText}>
+          {current.temp_c}&#176; | Feels like{" "}
+          <span className={styles.leftTextSpan}>{current.temp_c}&#176;</span>
+        </p>
       </div>
-      <div>
-        <span>{current.temp_c}&#176;</span>
-        <p>Partly cloudy</p>
+      <div className={styles.contentWrapper}>
+        <span className={styles.bigSizeDegree}>{current.temp_c}&#176;</span>
+        <p className={styles.rightText}>Partly cloudy</p>
         <p>Wind</p>
       </div>
     </div>
