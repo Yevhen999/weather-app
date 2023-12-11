8f2d4688f1c54da60a9f2737ab5ed70271ba20d9
unknown <leshchenkoyevhen@gmail.com>
2023-12-10 19:37:21 -0500

    add size to indicators icons

--- components/DetailedIndicators/DetailedIndicators.jsx
+++ components/DetailedIndicators/DetailedIndicators.jsx
@@ -6,25 +6,37 @@ import styles from "./DetailedIndicators.module.css";
 export const DetailedIndicators = () => {
   return (
     <section className={styles.section}>
-      DetailedIndicators
       <ul className={styles.indicatorsList}>
         <li className={styles.indicatorsItem}>
-          <IoPartlySunny color="#FFC100" className={styles.indicatorsIcon} />{" "}
+          <IoPartlySunny
+            size={20}
+            color="#FFC100"
+            className={styles.indicatorsIcon}
+          />{" "}
           Precipitation: <span>21%</span>
         </li>
         <li className={styles.indicatorsItem}>
-          <WiHumidity color="#FFC100" className={styles.indicatorsIcon} />
+          <WiHumidity
+            size={20}
+            color="#FFC100"
+            className={styles.indicatorsIcon}
+          />
           Humidity: <span>59%</span>
         </li>
         <li className={styles.indicatorsItem}>
           <WiNightCloudyWindy
+            size={20}
             color="#FFC100"
             className={styles.indicatorsIcon}
           />{" "}
           Wind: <span>10 km/h</span>
         </li>
         <li className={styles.indicatorsItem}>
-          <WiSunset color="#FFC100" className={styles.indicatorsIcon} />
+          <WiSunset
+            size={20}
+            color="#FFC100"
+            className={styles.indicatorsIcon}
+          />
           Sunset: <span>29%</span>
         </li>
       </ul>
