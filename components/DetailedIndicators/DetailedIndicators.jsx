184022bb4e18862059425af0df91e67a8a2bd076
unknown <leshchenkoyevhen@gmail.com>
2023-12-23 15:03:50 -0500

    add one more case to text render

--- components/DetailedIndicators/DetailedIndicators.jsx
+++ components/DetailedIndicators/DetailedIndicators.jsx
@@ -3,6 +3,7 @@ import { WiHumidity, WiNightCloudyWindy, WiSunset } from "react-icons/wi";
 import Image from "next/image";
 
 import partlySunny from "../../public/parlty-sunny.svg";
+import partlyRainy from "../../public/partly-rainy.svg";
 
 import styles from "./DetailedIndicators.module.css";
 
@@ -11,14 +12,35 @@ export const DetailedIndicators = ({ current, currentCondition }) => {
   7;
 
   const text = currentCondition.text;
+  console.log(text);
+
+  // switch (text) {
+  //   case "Mist":
+  //     return <Mist />;
+
+  //   default:
+  //     return console.log("first");
+  // }
+
+  // const USER_ROLE_ADMIN = "admin";
+  // const USER_ROLE_USER = "user";
+
+  // function Dashboard(props) {
+  //   const { userRole } = props;
+
+  //   switch (userRole) {
+  //     case USER_ROLE_ADMIN:
+  //       return <AdminDashboard />;
+  //     case USER_ROLE_USER:
+  //       return <UserDashboard />;
+  //     default:
+  //       return <div>Error: Invalid User Role</div>;
+  //   }
+  // }
 
   console.log(currentCondition);
 
-  // const imageLoader = ({ icon }) => {
-  //   return `https:${icon}`;
-  // };
-
-  console.log(icon);
+  // console.log(icon);
   return (
     <div className={styles.detailedIndicators}>
       <ul className={styles.indicatorsList}>
@@ -60,7 +82,8 @@ export const DetailedIndicators = ({ current, currentCondition }) => {
           </p>
         </li>
         <li className={styles.indicatorsItem}>
-          {text === "Overcast" ? (
+          {/* {conditionText} */}
+          {text === "Overcast" && (
             <Image
               src={partlySunny}
               alt="parlty-sunny"
@@ -68,10 +91,18 @@ export const DetailedIndicators = ({ current, currentCondition }) => {
               height={20}
               className={styles.indicatorsIcon}
             />
-          ) : (
-            <WiSunset
+            /* <WiSunset
               size={20}
               color="#FFC100"
+              className={styles.indicatorsIcon}
+            /> */
+          )}
+          {text === "Mist" && (
+            <Image
+              src={partlyRainy}
+              alt="parlty-rainy"
+              width={20}
+              height={20}
               className={styles.indicatorsIcon}
             />
           )}
