258ea46e5aa1c2eaa3a3b9a1255c54ae7057207d
unknown <leshchenkoyevhen@gmail.com>
2024-01-27 15:32:55 -0500

    add getStaticProps to Home

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,28 +6,51 @@ import { AppBar } from "@/components/AppBar/AppBar";
 import { Container } from "@/components/Container/Container";
 import { Today } from "@/components/Today/Today";
 
-const Home = () => {
-  const [weather, setWeather] = useState({});
-  const [current, setCurrent] = useState({});
-  const [currentCondition, setCurrentCondition] = useState({});
+export const getStaticProps = async () => {
+  const response = await axios.get(
+    "https://api.weatherapi.com/v1/current.json?key=4a06b0437031495ca6b20810233107&q=chicago"
+  );
+  const data = await response.data;
 
-  useEffect(() => {
-    const currentWeather = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.weatherapi.com/v1/current.json?key=4a06b0437031495ca6b20810233107&q=chicago"
-        );
-        const data = await response.data;
+  if (!data) {
+    return { notFound: true };
+  }
 
-        setWeather(data.location);
-        setCurrent(data.current);
-        setCurrentCondition(data.current.condition);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    currentWeather();
-  }, []);
+  const weather = await data.location;
+  const current = await data.current;
+  const currentCondition = await data.current.condition;
+
+  return {
+    props: {
+      weather,
+      current,
+      currentCondition,
+    },
+  };
+};
+
+const Home = ({ weather, current, currentCondition }) => {
+  // const [weather, setWeather] = useState({});
+  // const [current, setCurrent] = useState({});
+  // const [currentCondition, setCurrentCondition] = useState({});
+
+  // useEffect(() => {
+  //   const currentWeather = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         "https://api.weatherapi.com/v1/current.json?key=4a06b0437031495ca6b20810233107&q=chicago"
+  //       );
+  //       const data = await response.data;
+
+  //       setWeather(data.location);
+  //       setCurrent(data.current);
+  //       setCurrentCondition(data.current.condition);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   currentWeather();
+  // }, []);
 
   return (
     <main>
